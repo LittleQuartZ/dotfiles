313f1e5d0f2139e9f46d2680026aedf4c925042a
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2022-04-30 05:05:52 +0700

    polybars
    enable ipc

--- cfg/polybar/launch.sh
+++ cfg/polybar/launch.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+export XDG_RUNTIME_DIR="/run/user/1000"
+
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
+polybar-msg cmd quit
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
@@ -9,10 +12,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    export MONITOR=$m
-    export TRAY_POSITION=none
-    if [[ $m == "eDP" ]]; then
-     export TRAY_POSITION=right 
-    fi
-    polybar $1 -c ~/.config/polybar/config.ini &
-  done
+  export MONITOR=$m
+  export TRAY_POSITION=none
+  if [[ $m == "eDP" ]]; then
+    export TRAY_POSITION=right
+  fi
+  polybar "$1" -c ~/.config/polybar/config.ini 2>&1 | tee -a "/tmp/polybar_$1.log" &
+  disown
+done
