cad4c56e4996bda667719e79ea78355066022bb4
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2022-06-23 05:59:27 +0700

    init.lua: add autocmd for folds in html

--- cfg/customchad/init.lua
+++ cfg/customchad/init.lua
@@ -1,3 +1,8 @@
 -- example file i.e lua/custom/init.lua
 
+vim.cmd [[augroup remember_folds
+  autocmd!
+  autocmd BufWinLeave *.html mkview
+  autocmd BufWinEnter *.html silent! loadview
+augroup END]]
 -- load your globals, autocmds here or anything .__.
